21f57002f61fd39946083586fc515627ba0af651
amirulhabbibsiswanda <amirulhabbib458@gmail.com>
2025-05-27 18:19:43 +0700

    create database and try make migration

---
+++ folder-server/models/user.js
@@ -0,0 +1,67 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "username is required"
+        },
+        notNull: {
+          msg: "username is required"
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "email is required"
+        },
+        notNull: {
+          msg: "email is required"
+        },
+        isEmail: {
+          msg: "input must be email formatted"
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "password is required"
+        },
+        notNull: {
+          msg: "password is required"
+        }
+      }
+    },
+    mobileLegendsRank: {
+      type: DataTypes.STRING,
+      defaultValue: "EPIC"
+    },
+
+    favouriteRole: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
