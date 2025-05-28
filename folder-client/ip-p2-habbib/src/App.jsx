59b4f2c5675932fe327ee62a2caab891dcf5a0e2
amirulhabbibsiswanda <amirulhabbib458@gmail.com>
2025-05-29 03:37:35 +0700

    npm create vite

--- folder-client/ip-p2-habbib/src/App.jsx
+++ folder-client/ip-p2-habbib/src/App.jsx
@@ -2,34 +2,37 @@ import { useEffect } from "react";
 import axios from "axios";
 
 function App() {
-
-
-
   useEffect(() => {
     window.google.accounts.id.initialize({
       client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
       callback: async (response) => {
-        console.log("Encoded JWT ID token: " + response.credential);
+        try {
+          const id_token = response.credential;
+          console.log("Encoded JWT ID token: ", id_token);
 
-        const { data } = await axios.post("http://localhost:3000/users/login-google", {
-          googleToken: response.credential,
-        });
-        localStorage.setItem("access_token", data.access_token);
+          const { data } = await axios.post("http://localhost:3000/users/login-google", {
+            id_token,
+          });
+
+          console.log(data, "Access token dari backend");
+          localStorage.setItem("access_token", data.access_token);
+        } catch (error) {
+          console.error("Google login error:", error.response?.data || error.message);
+        }
       },
     });
 
-    window.google.accounts.id.renderButton(
-      // The ID of the HTML element where the button will be rendered
-      document.getElementById("buttonDiv"),
-      // Customize the button attributes
-      { theme: "outline", size: "large" }
-    );
+    window.google.accounts.id.renderButton(document.getElementById("buttonDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+
     window.google.accounts.id.prompt();
   }, []);
 
-
-
-  return <div id="buttonDiv"></div>;
+  return (
+    <div id="buttonDiv"></div>
+  )
 }
 
 export default App;
